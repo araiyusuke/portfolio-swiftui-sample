999e6ad761fb9a877e4e77f0251aa2cb5fbe638c
araiyusuke <info.yusuke.arai@gmail.com>
2022-07-01 09:26:08 +0900

    Initial Commit

---
+++ TapSimpleButton/TapSimpleButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapSimpleButtonApp.swift
+//  TapSimpleButton
+//
+//  Created by 名前なし on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct TapSimpleButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
