25e2901f43afc5c952ece4960138f7102fa9cf90
araiyusuke <info.yusuke.arai@gmail.com>
2022-07-20 07:48:26 +0900

    add: 取引検索の条件画面にデザインを反映

--- mobile/PortfolioSwiftUI/Screen/SearchTransactionScreen/SearchTransactionScreen.swift
+++ mobile/PortfolioSwiftUI/Screen/SearchTransactionScreen/SearchTransactionScreen.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct SearchTransactionScreen: View {
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var header: HeaderManager
     var body: some View {
+        VStack {
+
+            HStack {
+                rowLabel("検索条件")
+                Spacer()
+            }
+            .adjustSize(height: 35)
+            .padding(adjust(10))
+
+            List {
+                HStack {
+                    rowLabel("科目")
+                    Spacer()
+                    rowLabel("すべて")
+                }
+                HStack {
+                    rowLabel("取引日(いつから)")
+                    Spacer()
+                    rowLabel("2021/01/01")
+                }
+                HStack {
+                    rowLabel("取引日(いつまで)")
+                    Spacer()
+                    rowLabel("2021/07/20")
+                }
+            }
+            .listStyle(.plain)
+            .adjustSize(height: 200)
+            searchButton
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .background(Color.rgb(240, 240, 240))
+    }
+
+    func rowLabel(_ title: String) -> some View {
+        Text(title)
+            .customFont(size: 14, spacing: .none, rgb: Asset.lightGray.color, weight: .light)
+    }
+    var searchButton: some View {
         Text("検索")
+            .customFont(size: 16, spacing: .long, rgb: Asset.lightBlue.color, weight: .light)
+            .adjustPadding(.all, 13)
+            .frame(maxWidth: .infinity)
+            .background(.white)
     }
 }
 
